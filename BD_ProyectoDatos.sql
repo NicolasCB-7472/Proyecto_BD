ff1bb1ee4fa74ec8ca50df14ceb2ba77e8fafdd0
Nicolas <bardanca.nicolas@gmail.com>
2025-05-04 20:19:51 -0300

    Se agregan los archivos del proyecto, (no finalizados)

---
+++ BD_ProyectoDatos.sql
@@ -0,0 +1,17 @@
+#---------Parte 0 - Datos a insertar ---------# 
+
+ INSERT INTO FIGURAS(FIGURA_ID, FIGURA_NAME, FIGURA_TIPO) VALUES(
+	('F00001', 'TrianguloEquilatero1', '3'),
+    ('F00002', 'TrianguloEquilatero2', '3'),
+    ('F00003', 'TrianguloEquilatero3', '3'),
+    ('F00004', 'TrianguloIsoceles1', '4'),
+    ('F00005', 'TrianguloIsoceles2', '4'),
+    ('F00006', 'TrianguloEscaleno1', '5'),
+    ('F00007', 'Cuadrado1', '2'),
+    ('F00006', 'Cuadrado2', '2'),
+    ('F00006', 'Cuadrado3', '2'),
+    ('F00006', 'Cuadrado4', '2'),
+    ('F00006', 'Circulo1', '1'),
+    ('F00006', 'Circulo2', '1'),
+    ('F00006', 'Circulo3', '1')
+);
