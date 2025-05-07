eef7553b38ca2f5ace6cb3a6fe727a7839c1bb05
Nicolas <bardanca.nicolas@gmail.com>
2025-05-07 01:41:05 -0300

    Se termina el modulo de triggers, y se corrigen irregularidades en ProyectoFiguras

--- BD_Disparadores.sql
+++ BD_Disparadores.sql
@@ -1,12 +1,27 @@
 #---------Parte 3 - Triggers ---------# 
 #1 Se insertan datos no validos en figuras
+DELIMITER //
+CREATE TRIGGER BI_ValidFiguras BEFORE INSERT ON FIGURAS
+FOR EACH ROW
+BEGIN
+	-- ID is a code 6 char code (FXXXXX) and FIGURA_TIPO is capped to 10 figures
+	IF (NEW.ID NOT REGEXP '^F[0-9]{5}$' OR NEW.FIGURA_TIPO > 10) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='La insercion en figuras no es valida';
+    END IF;
+END;
+//
+DELIMITER ;
+
 #2 Se pasa del limite de figuras
 DELIMITER //
 CREATE TRIGGER BI_LimiteFiguras BEFORE INSERT ON FIGURAS
 FOR EACH ROW
 BEGIN
+	DECLARE total_Figuras INT;
+    SELECT COUNT(*) INTO total_Figuras FROM FIGURAS;
+	
 	#100 is a magic value, could be a global constant, predifined number, or derivated one, etc
-	IF ((SELECT COUNT(F.ID) FROM FIGURAS AS F) > 100 )THEN
+	IF (total_Figuras > 100 )THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='El limite de figuras se ha alcanzado';
     END IF;
 END;
@@ -20,6 +35,7 @@ CREATE TABLE AUDIT_CIRCULO(
     CONSTRAINT AUDIT_PK PRIMARY KEY (ID)
 );
 -- Se crea un trigger para evitar la eliminacion de tablas, modificacion de estas, etc
+-- Se puede revocar los permisos necesarios de las tablas y hacerla read-only
 DELIMITER //
 CREATE TRIGGER BD_AUDIT_CIR BEFORE DELETE ON AUDIT_CIRCULO
 FOR EACH ROW
@@ -29,27 +45,21 @@ END;
 //
 DELIMITER ;
 
-CREATE TABLE CIRCULO ( 
-	ID_CIRCULO INTEGER NOT NULL, 
-    RADIUS FLOAT, 
-    CONSTRAINT CIRCULO_PK PRIMARY KEY (ID_CIRCULO) 
-); 
-
 -- Se crea el trigger en si
 DELIMITER //
 CREATE TRIGGER AI_AUDIT_CIR AFTER INSERT ON CIRCULO
 FOR EACH ROW
 BEGIN
 	INSERT INTO AUDIT_CIRCULO(TEXTO) VALUES(
-		'Figura: ',
-        CAST(OLD.ID as CHAR(10)),
-        'Viejo Radio: ',
-        CAST(OLD.ID as CHAR(10)),
-        'Nuevo Radio: ',
-        CAST(NEW.ID as CHAR(10)),
-        'Fecha: ',
-        CAST(TIMESTAMP as CHAR(20))
-        -- Se podria agregar el usuario tambien
+		CONCAT(
+			'Figura: ',
+			CAST(NEW.ID_CIRCULO as CHAR(10)),
+			'Radio: ',
+			CAST(NEW.RADIUS as CHAR(10)),
+			'Fecha: ',
+			CAST(CURRENT_TIMESTAMP as CHAR(20))
+			-- Se podria agregar el usuario tambien
+		)
     );
 END;
 //
