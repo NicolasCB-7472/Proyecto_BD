5e1a062e09755919c8fb035cc1baa26ea3437984
Nicolas <bardanca.nicolas@gmail.com>
2025-05-18 00:52:52 -0300

    Se hacen unas modificaciones a algunas tablas y consignas, se plantea el procedimiento 2

--- BD_Procedimientos.sql
+++ BD_Procedimientos.sql
@@ -16,28 +16,56 @@ CREATE PROCEDURE crear_cuadrado(IN lado FLOAT)
             CONCAT('C', CAST(id_fig as CHAR(5))),
             1
         );
+        
+        # No se agrega a FIGURAS_INDICE (el ejercicio no lo aborda)
     END
 //
 DELIMITER ;
 
-#2 Inserta todas las figuras de FIGURAS en Indice
+#2 Inserta todas las figuras de FIGURAS en Indice con uso de cursor.
+
+#Se crea un cursor con la info de figuras
+	# -> Se inserta FIGURAX_ID, ID, y TIPO
+	# -> Se busca en FIGURAS_COLECCION correspondientemente con la figura si pertenece alguna coleccion
+
+
 #3 Agrega 3 figuras (IDs) a figura coleccion
-CREATE TABLE FIGURAS (
-	ID INTEGER NOT NULL AUTO_INCREMENT,
-	FIGURA_ID VARCHAR(6) NOT NULL,
-    #(1) pertenece a coleccion (0) no pertenece a coleccion
-	FIGURA_COLECCION TINYINT DEFAULT 0,
-	FIGURA_NAME VARCHAR(10),
-	FIGURA_TIPO TINYINT, 
-	CONSTRAINT FIGURAS_PK PRIMARY KEY (ID) 
- ); 
 
 DELIMITER //
-CREATE PROCEDURE crear_coleccion(IN id_cuad VARCHAR(6), IN id_tri VARCHAR(6), IN id_cir VARCHAR(6))
+CREATE PROCEDURE crear_coleccion(IN id_cuad VARCHAR(6), IN id_tri VARCHAR(6), IN id_cir VARCHAR(6), IN col_name VARCHAR(6))
 	BEGIN
 		#Se toma la figura y actualiza coleccion
+        DECLARE idcua INTEGER;
+        DECLARE idtri INTEGER;
+        DECLARE idcir INTEGER;
+        DECLARE idcol INTEGER;
+        
+        #Suponiendo que estan ya en las tablas
+        UPDATE FIGURAS AS F 
+        SET F.FIGURA_COLECCION=1 
+        WHERE F.FIGURA_ID=id_cuad OR F.FIGURA_ID=id_tri OR F.FIGURA_ID=id_cir; 
+        
+        SELECT SUBSTRING(id_cuad, 2, 6) INTO idcua;
+        SELECT SUBSTRING(id_tri, 2, 6) INTO idtri;
+        SELECT SUBSTRING(id_cir, 2, 6) INTO idcir;
+        
         #Se agrega efectivamente a la coleccion
+        INSERT INTO FIGURAS_COLECCION(FIGURAC_ID, ID_CUADRADO, ID_TRIANGULO, ID_CIRCULO) VALUES(
+			col_name,
+            idcua,
+            idtri,
+            idcir
+        );
+        
         #Se actualiza figuras indice con la coleccion
+        #Suponiendo que siempre se inserta al final y no hay eliminaciones
+        SELECT LAST(ID_COLECCION) INTO idcol FROM FIGURAS_COLECCION;
+        
+        UPDATE FIGURAS_INDICE AS FI
+        SET COLX_ID=idcol
+        WHERE (FI.ID=idcua AND FI.FIGURAX_ID=id_cuad) OR
+        (FI.ID=idtri AND FI.FIGURAX_ID=id_tri) OR
+        (FI.ID=idcir AND FI.FIGURAX_ID=id_cir);
     END
 //
 
